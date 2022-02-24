8f56bdc3e675ead32523e5f0878ce463b5ea70bd
lvhaizhou <417703682@qq.com>
2022-02-24 11:18:53 +0800

    实现 reactive 和 readonly 嵌套对象转换 功能

---
+++ src5/reactivity/baseHandlers.ts
@@ -0,0 +1,50 @@
+import { isObject } from "../shared";
+import { track, trigger } from "./effect";
+import { reactive, ReactiveFlags, readonly } from "./reactive";
+
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    }
+
+    const res = Reflect.get(target, key);
+
+    // 看看 res 是不是一个 object
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res);
+    }
+
+    if (!isReadonly) {
+      track(target, key);
+    }
+    return res;
+  };
+}
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+
+    trigger(target, key);
+    return res;
+  };
+}
+
+export const mutableHandlers = {
+  get,
+  set,
+};
+
+export const readonlyHanders = {
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn("不能调用");
+    return true;
+  },
+};
---
+++ src5/reactivity/index.ts
@@ -0,0 +1,2 @@
+// 版本5:
+// （1）实现 reactive 和 readonly 嵌套对象转换 功能
