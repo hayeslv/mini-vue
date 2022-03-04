32f05cc7682a5bc2110bc754fa301205b81c72bb
lvhaizhou <417703682@qq.com>
2022-03-04 10:50:52 +0800

    stop、onStop

--- testSrc/reactivity/effect.ts
+++ testSrc/reactivity/effect.ts
@@ -1,5 +1,10 @@
+import { extend } from "./shared";
+
 class ReactiveEffect {
   private _fn: any;
+  public deps = [];
+  active = true;
+  onStop?: () => void;
   constructor(fn, public scheduler?) {
     this._fn = fn;
   }
@@ -7,6 +12,21 @@ class ReactiveEffect {
     activeEffect = this;
     return this._fn();
   }
+  stop() {
+    if (this.active) {
+      clearupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function clearupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
 }
 
 const targetMap = new Map();
@@ -25,7 +45,9 @@ export function track(target, key) {
     depsMap.set(key, dep);
   }
 
+  if (!activeEffect) return;
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
@@ -41,12 +63,18 @@ export function trigger(target, key) {
   }
 }
 
+export function stop(runner) {
+  runner.effect.stop();
+}
+
 export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler);
+  extend(_effect, options);
 
   _effect.run();
 
-  const runner = _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
 
   return runner;
 }
