77e946617fe77da7752a4251cb1ff12fb6c14e5f
lvhaizhou <417703682@qq.com>
2022-03-04 10:17:50 +0800

    clear

--- testSrc/share/index.ts
+++
@@ -1,9 +0,0 @@
-export const extend = Object.assign;
-
-export const isObject = (value) => {
-  return value !== null && typeof value === "object";
-};
-
-export const hasChanged = (newValue, value) => {
-  return !Object.is(newValue, value);
-};
