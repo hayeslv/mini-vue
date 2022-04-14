59332079e78e1fd15b0072ace843c6fb6b8a488b
lvhaizhou <417703682@qq.com>
2022-04-14 17:39:53 +0800

    实现自定义渲染器：custom-renderer

---
+++ example/customRenderer/main.js
@@ -0,0 +1,34 @@
+import { createRenderer } from '../../lib/guide-mini-vue.esm.js'
+import { App } from './App.js'
+// canvas-render：pixijs
+// console.log(PIXI);
+
+const game = new PIXI.Application({
+  width: 500,
+  height: 500
+})
+document.body.append(game.view)
+
+const renderer = createRenderer({
+  createElement(type) {
+    if(type === "rect") {
+      const rect = new PIXI.Graphics()
+      rect.beginFill(0xff0000)
+      rect.drawRect(0, 0, 100, 100)
+      rect.endFill()
+
+      return rect
+    }
+  },
+  patchProp(el, key, value) {
+    el[key] = value
+  },
+  insert(el, parent) {
+    parent.addChild(el)
+  }
+})
+
+renderer.createApp(App).mount(game.stage)
+
+// const rootContainer = document.querySelector("#app");
+// createApp(App).mount(rootContainer);
--- src/runtime-core/createApp.ts
+++ src/runtime-core/createApp.ts
@@ -1,14 +1,15 @@
-import { render } from "./render"
 import { createVNode } from "./vnode"
 
-export function createApp(rootComponent) {
-  return {
-    mount(rootContainer) {
-      // 先转换成 vnode
-      // component -> vnode
-      const vnode = createVNode(rootComponent)
-
-      render(vnode, rootContainer)
+export function createAppApi(render){
+  return function createApp(rootComponent) {
+    return {
+      mount(rootContainer) {
+        // 先转换成 vnode
+        // component -> vnode
+        const vnode = createVNode(rootComponent)
+  
+        render(vnode, rootContainer)
+      }
     }
   }
 }
