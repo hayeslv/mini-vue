82bb24c7c26de0089d60e8b60b05c7bcc9e7e1ee
lvhaizhou <417703682@qq.com>
2022-05-16 16:41:54 +0800

    更新element的props

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,5 +1,5 @@
 import { effect } from "../reactivity/effect";
-import { isObject } from "../shared/index";
+import { EMPTY_OBJ, isObject } from "../shared/index";
 import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./component"
 import { createAppApi } from "./createApp";
@@ -61,6 +61,37 @@ export function createRenderer(options){
 
   function patchElement(n1, n2, container) {
     console.log("patchElement");
+    console.log("n1:", n1);
+    console.log("n2:", n2);
+
+    const oldProps = n1.props || EMPTY_OBJ
+    const newProps = n2.props || EMPTY_OBJ
+
+    const el = n2.el = n1.el
+
+    patchProps(el, oldProps, newProps)
+  }
+
+  
+
+  function patchProps(el, oldProps, newProps) {
+    if(oldProps !== newProps) {
+      for (const key in newProps) {
+        const prevProp = oldProps[key]
+        const nextProp = newProps[key]
+        if(prevProp !== nextProp) {
+          hostPatchProp(el, key, prevProp, nextProp)
+        }
+      }
+
+      if(oldProps !== EMPTY_OBJ) {
+        for(const key in oldProps) {
+          if(!(key in newProps)) {
+            hostPatchProp(el, key, oldProps[key], null)
+          }
+        }
+      }
+    }
   }
   
   function mountElement(vnode, container, parentComponent) {
@@ -78,17 +109,8 @@ export function createRenderer(options){
     for (const key in props) {
       const val = props[key]
   
-      // 判断是否是事件的命名规范
-      // const isOn = (key: string) => /^on[A-Z]/.test(key);
-      // if (isOn(key)) {
-      //   const event = key.slice(2).toLowerCase()
-      //   el.addEventListener(event, val)
-      // } else {
-      //   el.setAttribute(key, val)
-      // }
-  
       // 通用
-      hostPatchProp(el, key, val)
+      hostPatchProp(el, key, null, val)
     }
   
     // 挂载到 container
