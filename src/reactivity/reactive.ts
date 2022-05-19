ccfe15211023f1033adc3c8f2c4e24981196c23e
lvhaizhou <417703682@qq.com>
2022-05-19 16:45:40 +0800

    reactive---代码重构

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,31 +1,39 @@
 import { track, trigger } from "./effect";
 
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key)
+    
+    if(!isReadonly) {
+      // 依赖收集
+      track(target, key)
+    }
+
+    return res;
+  }
+}
+
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value)
+
+    // 触发依赖
+    trigger(target, key)
+    return res
+  }
+}
+
 // reactive其实就是 Proxy 的代理
 export function reactive(raw) {
   return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
-
-      // 依赖收集
-      track(target, key)
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value)
-
-      // 触发依赖
-      trigger(target, key)
-      return res
-    }
+    get: createGetter(),
+    set: createSetter()
   })
 }
 
 export function readonly(raw) {
   return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      return res;
-    },
+    get: createGetter(true),
     set(target, key, value) {
       console.warn(`readonly不能赋值：${target}`)
       return true
