9860a44d0eb621c47fd09c063610a0d7f354f0d4
lvhaizhou <417703682@qq.com>
2022-05-20 16:56:26 +0800

    computed--懒执行

--- src/reactivity/tests/computed.spec.ts
+++ src/reactivity/tests/computed.spec.ts
@@ -11,4 +11,26 @@ describe("computed", () => {
     });
     expect(age.value).toBe(1);
   });
+  it("should compute lazily", () => {
+    // 计算属性可以缓存
+    const value = reactive({
+      foo: 1,
+    });
+    const getter = jest.fn(() => {
+      return value.foo;
+    });
+    const cValue = computed(getter);
+
+    // 懒执行：如果没有调用cValue.value的话，getter不会调用
+    expect(getter).not.toHaveBeenCalled();
+
+    expect(cValue.value).toBe(1);
+    expect(getter).toHaveBeenCalledTimes(1);
+
+    // 当我们再次触发get操作，需要验证 getter 还是只被调用了一次
+    cValue.value;
+    expect(getter).toHaveBeenCalledTimes(1);
+
+    
+  });
 });
