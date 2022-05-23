ee3cfd9dcf1a4a5d66162eee8cce5974fe7b2fbc
lvhaizhou <417703682@qq.com>
2022-05-23 17:17:22 +0800

    实现组件emit功能--抽离功能函数

--- src/runtime-core/componentEmit.ts
+++ src/runtime-core/componentEmit.ts
@@ -1,19 +1,10 @@
+import { camelize, toHandlerKey } from "../shared"
+
 export function emit(instance, event, ...args){ // args：用户传参
   const { props } = instance
 
-  // 烤串转驼峰
-  const camelize = (str: string) => str.replace(/-(\w)/g, (_, c: string) => {
-    // _：匹配的内容 -(\w)； c：(\w) 的内容
-    return c ? c.toUpperCase() : ""
-  })
-
-  // 首字母大写
-  const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
-
-  const toHandlerKey = (str: string) => str ? "on" + capitalize(str) : ""
-
   const handlerName = toHandlerKey(camelize(event)) // 加入烤串转驼峰方法
-  
+
   const handler = props[handlerName]
 
   handler && handler(...args)
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,8 +1,5 @@
 export * from "./ShapeFlags"
 
-
-
-
 export const extend = Object.assign
 
 export const isObject = (value) => {
@@ -14,3 +11,15 @@ export const hasChanged = (value, newValue) => {
 }
 
 export const hasOwn = (object, key) => Object.prototype.hasOwnProperty.call(object, key);
+
+
+// 烤串转驼峰
+export const camelize = (str: string) => str.replace(/-(\w)/g, (_, c: string) => {
+  // _：匹配的内容 -(\w)； c：(\w) 的内容
+  return c ? c.toUpperCase() : ""
+})
+
+// 首字母大写
+export const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const toHandlerKey = (str: string) => str ? "on" + capitalize(str) : ""
