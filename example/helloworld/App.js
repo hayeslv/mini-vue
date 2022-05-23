cfd8e29c1ded1256173edba9b751967c641ab6f8
lvhaizhou <417703682@qq.com>
2022-05-23 14:40:26 +0800

    初始化element主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -3,7 +3,22 @@ import { h } from '../../lib/guide-mini-vue.esm.js'
 export const App = {
   name: "App",
   render() {
-    return h("div", "hi, mini-vue")
+    // 添加第二个参数：props
+    return h(
+      "div", 
+      {
+        id: "root",
+        // class: "red"
+      }, 
+      // String类型
+      // "hi, mini-vue"
+      // Array类型
+      [
+        h("p", { class: "red" }, "hi"),
+        h("p", { class: "blue" }, "mini-vue"),
+      ]
+    )
+    
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from './../shared/index';
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -5,12 +6,15 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  // TODO 处理组件
-  processComponent(vnode, container)
+  console.log(vnode.type);
+  if (typeof vnode.type === "string") {
+    // 处理元素
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    // 处理组件
+    processComponent(vnode, container)
+  }
 
-
-  // TODO 处理元素
-  // processElement()
 }
 
 function processComponent(vnode, container) {
@@ -18,6 +22,15 @@ function processComponent(vnode, container) {
   mountComponent(vnode, container)
   // TODO 更新组件
 }
+
+function processElement(vnode, container) {
+  // element 类型也分为 mount 和 update，这里先实现mount
+  mountElement(vnode, container)
+
+  // TODO 更新element
+  // updateElement()
+}
+
 
 function mountComponent(vnode: any, container: any) {
   // 抽离出 instance 实例，表示组件实例
@@ -28,10 +41,30 @@ function mountComponent(vnode: any, container: any) {
   setupRenderEffect(instance, container)
 }
 
+function mountElement(vnode, container) {
+  const el = document.createElement(vnode.type)
+  // children可能是：string、array
+  const { props, children }  = vnode
+
+  if(typeof children === "string") {
+    el.textContent = children
+  } else if(Array.isArray(children)) {
+    // children 中每个都是 vnode，需要继续调用 patch，来判断是element类型还是component类型，并对其初始化
+    children.forEach(v => patch(v, el))
+  }
+
+  // props
+  for (const key in props) {
+    const value = props[key]
+    el.setAttribute(key, value)
+  }
+
+  container.append(el)
+}
+
 function setupRenderEffect(instance, container) {
   // 获取render函数的返回值（返回的是组件的虚拟节点树）
   const subTree = instance.render()
   // 基于返回的虚拟节点，对其进行patch比对（打补丁）
   patch(subTree, container)
 }
-
